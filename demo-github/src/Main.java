d86862ff6f134ceb181fca596b0e6ccc3af35a96
Vadim <ubuntutranslate@gmail.com>
2024-09-23 21:11:02 +0300

    Update Main.java

--- demo-github/src/Main.java
+++ demo-github/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         method();
+        System.out.println("We have a conflict");
     }
 
     public static void method() {
